926e3b17ab575cc78930a47f6ae6fa2f8b17c4f2
bwnstk <info@flimmerkiste.xyz>
2020-11-10 11:08:50 +0100

    :sparkles: Add Inquirer

--- app.js
+++ app.js
@@ -7,12 +7,58 @@ const wifi = {
 // if (args["pwd"] === "wifi") console.log(wifi);
 
 // ! 2.Try with readline
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-readline.question(`Show password for... `, (name) => {
-  if (name === "wifi") console.log(wifi);
-  readline.close();
-});
+// readline.question(`Show password for... `, (name) => {
+//   if (name === "wifi") console.log(wifi);
+//   readline.close();
+// });
+
+// ! Inquirer.js
+
+const inquirer = require("inquirer");
+
+var questions = [
+  {
+    type: "input",
+    name: "pwd",
+    message: "Show password for?",
+  },
+];
+var masterPassword = [
+  {
+    type: "password",
+    name: "masterPwd",
+    message: "Enter MasterPassword:",
+  },
+];
+
+function startPasswordManager() {
+  inquirer.prompt(masterPassword).then((answer) => {
+    if (answer["masterPwd"] === "admin") {
+      (function searchDB() {
+        inquirer
+          .prompt(questions)
+          .then((answers) => {
+            if (answers["pwd"] === "wifi") {
+              console.log(wifi);
+            } else {
+              console.log("No password safed... try again");
+              searchDB();
+            }
+          })
+          .catch((error) => {
+            console.log("error", error);
+          });
+      })();
+    } else {
+      console.log("so stupid... try again");
+      startPasswordManager();
+    }
+  });
+}
+
+startPasswordManager();
