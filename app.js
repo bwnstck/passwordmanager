e1545e7dcbd88f6a65d67f1d35717fde5887ca5c
bwnstk <info@flimmerkiste.xyz>
2020-11-10 10:43:02 +0100

    :passport_control: Add readline method

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const args = require("minimist")(process.argv.slice(2));
-console.log(args["name"]);
+const wifi = {
+  name: "MartinRouterKing",
+  pwd: "a0f-2faw-vajv3-a0a-",
+};
+// ! First Try with minimist
+// const args = require("minimist")(process.argv.slice(2));
+// if (args["pwd"] === "wifi") console.log(wifi);
+
+// ! 2.Try with readline
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`Show password for... `, (name) => {
+  if (name === "wifi") console.log(wifi);
+  readline.close();
+});
