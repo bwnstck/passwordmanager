7c0a48da8addcbcd71cbc391a2b6a1a5d5d4a66e
bwnstk <info@flimmerkiste.xyz>
2020-11-18 00:33:26 +0100

    :bug: Fix incoming data notbeing an array

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,64 @@
 import { useState } from "react";
 import styled from "styled-components/macro";
 import "./App.css";
-
+import crypto from "../../utils/crypto";
 const PasswordSearchForm = styled.form`
   width: 80%;
 `;
-
-async function getPasswordsExpress() {
-  const request = await fetch("http://localhost:3001/api/passwords/");
-  const data = await request.json();
-  return data;
-}
+const PasswordInput = styled.input``;
 
 function App() {
-  const [passwords, setPasswords] = useState([]);
+  const [searchEntries, setSearchEntries] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function decryptEntries(entries) {
+    const encryptedEntries = entries.map((entry) => {
+      entry.pwd = crypto.decrypt(entry.pwd, process.env.CRYPTO_PWD);
+      entry.email = crypto.decrypt(entry.email, process.env.CRYPTO_PWD);
+    });
+    return encryptedEntries;
+  }
+
+  const handleSubmit = async (inputValue) => {
+    const passwords = await getSpecificEntryExpress(inputValue);
+    const encryptedEntries = decryptEntries(passwords);
+    setSearchEntries(encryptedEntries);
+  };
 
   return (
     <div className="App">
-      <PasswordSearchForm />
-      <button
-        onClick={async () => {
-          setPasswords(await getPasswordsExpress());
+      <PasswordSearchForm
+        onSubmit={async (event) => {
+          event.preventDefault();
+          await handleSubmit(inputValue);
         }}
       >
-        Click me
+        <PasswordInput
+          type="text"
+          placeholder="🕵️ Search your entries"
+          onChange={(event) => {
+            event.preventDefault();
+            setInputValue(event.target.value);
+          }}
+        />
+        <button onClick={async () => {}}>Click me</button>
+      </PasswordSearchForm>
+      <button
+        onClick={async () => {
+          setSearchEntries(await getAllPwdsExpress());
+        }}
+      >
+        Show all
       </button>
       <div className="output">
-        {passwords &&
-          passwords.map((pwd) => {
-            return <li key={pwd._id}>{pwd.title}</li>;
+        {searchEntries &&
+          searchEntries.map((pwd) => {
+            return (
+              <li key={pwd._id}>
+                <h5>{pwd.title}</h5>
+                <h6>{pwd.pwd}</h6>
+              </li>
+            );
           })}
       </div>
     </div>
@@ -36,3 +66,14 @@ function App() {
 }
 
 export default App;
+
+async function getAllPwdsExpress() {
+  const request = await fetch("http://localhost:3001/api/passwords/");
+  const data = await request.json();
+  return data;
+}
+async function getSpecificEntryExpress(input) {
+  const request = await fetch(`http://localhost:3001/api/passwords/${input}`);
+  const data = await request.json();
+  return [data];
+}
